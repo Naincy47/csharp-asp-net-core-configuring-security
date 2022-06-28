d51758496905e04cfa3998907807db44f5f6aeba
NICEDEN\najain <naincy.jain@nice.com>
2022-06-28 12:53:33 +0530

    complete project

--- ConferenceTracker/Startup.cs
+++ ConferenceTracker/Startup.cs
@@ -2,19 +2,22 @@ using ConferenceTracker.Data;
 using ConferenceTracker.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using Microsoft.AspNetCore.Http;
 
 namespace ConferenceTracker
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+		private readonly string _allowedOrigins = "_allowedOrigins";
+		
+		public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
@@ -26,25 +29,41 @@ namespace ConferenceTracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            SecretMessage = Configuration["SecretMessage"];
             services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("ConferenceTracker"));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.Configure<CookiePolicyOptions>(options => { options.CheckConsentNeeded = context => true; options.MinimumSameSitePolicy = SameSiteMode.None; });
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddTransient<IPresentationRepository, PresentationRepository>();
             services.AddTransient<ISpeakerRepository, SpeakerRepository>();
+            services.AddCors(options => { options.AddPolicy(_allowedOrigins, builder => { builder.WithOrigins("http://pluralsight.com"); }); });
         }
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {            
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        {
+			if (!env.IsDevelopment())
+			{
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+			}
+			else
+			{
+                logger.LogInformation("Environment is in development");
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+			}
+
             using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
             using (var context = scope.ServiceProvider.GetService<ApplicationDbContext>())
                 context.Database.EnsureCreated();
-
+            app.UseCors(_allowedOrigins);
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseCookiePolicy();
             app.UseRouting();
 
             app.UseAuthentication();
@@ -58,5 +77,6 @@ namespace ConferenceTracker
                 endpoints.MapRazorPages();
             });
         }
+
     }
 }
